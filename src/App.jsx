27b17998e9ff3d24d47264fa4e4f573481d00e25
laksitha-s <152200148+laksitha-s@users.noreply.github.com>
2025-12-02 21:36:33 +0530

    Add 'Virtual Vogue' project to project list
    Added a new project 'Virtual Vogue' to the project list with relevant details.

--- src/App.jsx
+++ src/App.jsx
@@ -272,6 +272,15 @@ const App = () => {
         tags: ["Figma", "Canva", "UI Design", ],
         image: "/assets/quick bite.png",
       },
+     {
+        title: "Virtual Vogue",
+        description:
+          "Virtual Vogue is a modern, user-centric e-commerce UI project designed to enhance the online dress-shopping experience through Augmented Reality (AR) try-on technology.",
+        github: "#",
+        demo: "https://www.figma.com/proto/13vQr6tWANGYwL4sVYam2K/ar-ty-on-wireframe?page-id=0%3A1&node-id=160-1359&viewport=-1418%2C1149%2C0.21&t=bfNDHByeGnerbcIz-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=56%3A139&show-proto-sidebar=1",
+        tags: ["Figma", ,"UI","Design"],
+        image: "/assets/virtualVogue.jpg",
+      },
       {
         title: "Mood-Based Movie Recommender",
         description:
@@ -290,15 +299,7 @@ const App = () => {
         tags: ["Node.js", "Express", "MongoDB", "React"],
         image: "/assets/booklyt.png",
       },
-      {
-        title: "Virtual Vogue",
-        description:
-          "Virtual Vogue is a modern, user-centric e-commerce UI project designed to enhance the online dress-shopping experience through Augmented Reality (AR) try-on technology.",
-        github: "#",
-        demo: "https://www.figma.com/proto/13vQr6tWANGYwL4sVYam2K/ar-ty-on-wireframe?page-id=0%3A1&node-id=160-1359&viewport=-1418%2C1149%2C0.21&t=bfNDHByeGnerbcIz-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=56%3A139&show-proto-sidebar=1",
-        tags: ["Figma", ,"UI","Design"],
-        image: "/assets/homes.png",
-      },
+      
     ]
 
     return (
